2f734e7b6fa93cbb1cb1527c21182c9501a1484c
admin <root@localhost>
2019-09-10 17:46:58 +0000

    updated IP_recon to have a prompt

--- Sorsnce_IP_Recon.py
+++ Sorsnce_IP_Recon.py
@@ -1,4 +1,5 @@
 """
+The following playbook allows for recon on any IPv4 Address.
 """
 
 import phantom.rules as phantom
@@ -47,7 +48,7 @@ def ip_reputation_1(action=None, success=None, container=None, results=None, han
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("ip reputation", parameters=parameters, assets=['virustotal_api'], callback=prompt_1, name="ip_reputation_1")
+    phantom.act("ip reputation", parameters=parameters, assets=['virustotal_api'], callback=filter_2, name="ip_reputation_1")
 
     return
 
@@ -62,7 +63,7 @@ Do you want to block this IP Address?"""
 
     # parameter list for template variable replacement
     parameters = [
-        "",
+        "artifact:*.cef.destinationAddress",
     ]
 
     #responses:
@@ -79,7 +80,43 @@ Do you want to block this IP Address?"""
         },
     ]
 
-    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters, response_types=response_types)
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="prompt_1", parameters=parameters, response_types=response_types, callback=filter_3)
+
+    return
+
+def filter_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_2() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["ip_reputation_1:action_result.data.*.detected_referrer_samples.*.positives", ">=", "1"],
+        ],
+        name="filter_2:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        prompt_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+def filter_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_3() called')
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        action_results=results,
+        conditions=[
+            ["prompt_1:action_result.summary.responses.0", "==", "Yes"],
+        ],
+        name="filter_3:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        pass
 
     return
 
